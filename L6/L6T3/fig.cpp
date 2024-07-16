1d6ebbbef7623e80d376b17326d676a2eb821912
mytevelev <mytevelev@gmail.com>
2024-07-16 15:24:58 +0300

    bad

---
+++ L6/L6T3/fig.cpp
@@ -0,0 +1,61 @@
+﻿#include "fig.h"
+using namespace std; //---------------- 
+
+// Публичный конструктор без параметров
+Figure::Figure() : sides_count(0), name("Фигура") {}
+
+// Публичный метод для получения количества сторон
+int Figure::get_sides_count() const 
+{
+    return sides_count;
+}
+
+// Публичный метод для получения имени фигуры
+string Figure::get_name() const 
+{
+    return name;
+}
+// getters ----------------------------------------
+int Figure::get_sA() const
+{
+    return sideA;
+}
+int Figure::get_sB() const
+{
+    return sideB;
+}
+int Figure::get_sC() const
+{
+    return sideC;
+}
+int Figure::get_sD() const
+{
+    return sideD;
+}
+int Figure::get_aA() const
+{
+    return angleA;
+}
+int Figure::get_aB() const
+{
+    return angleB;
+}
+int Figure::get_aC() const
+{
+    return angleC;
+}
+int Figure::get_aD() const
+{
+    return angleD;
+}
+
+ void Figure::printinfo()
+{
+    cout << get_name() << ": " << get_sides_count() << endl;
+}
+
+ bool Figure::check()
+{
+
+    return false;
+}
---
+++ L6/L6T3/fig.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <string>
+#include <iostream>
+
+
+class Figure {
+protected:
+    int sides_count;
+    string name;
+    int sideA = 100, sideB = 200, sideC = 300, sideD = 400,
+        angleA = 60, angleB = 60, angleC = 60, angleD = 60;
+
+    // Защищённый конструктор с параметром
+    Figure(int sides_count, string name);
+public:
+    // Публичный конструктор без параметров
+    Figure();
+
+    // Публичный метод для получения количества сторон
+    int get_sides_count() const;
+     // Публичный метод для получения имени фигуры
+    string get_name() const;
+    // getters ----------------------------------------
+    int get_sA() const;
+    int get_sB() const;
+    int get_sC() const;
+    int get_sD() const;
+    int get_aA() const;
+    int get_aB() const;
+    int get_aC() const;
+    int get_aD() const;
+
+    virtual void printinfo();
+
+    virtual bool check();
+
+};
