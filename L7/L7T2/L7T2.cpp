72a66c55ec1f9d42f0abc5333bcb7006e5df236b
mytevelev <mytevelev@gmail.com>
2024-07-17 12:33:36 +0300

    1

---
+++ L7/L7T2/L7T2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+// Определение макроса SUB для вычитания чисел
+#define SUB(x, y) ((x) - (y)) //скобки!
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    int a = 6;
+    int b = 5;
+    int c = 2;
+
+    std::cout << SUB(a, b) << std::endl;            // Ожидается 1
+    std::cout << SUB(a, b) * c << std::endl;        // Ожидается 2
+    std::cout << SUB(a, b + c) * c << std::endl;    // Ожидается -2
+
+    return 0;
+}
