7126fb4096495c3a7a5042726305ab0645d673ce
Maxim Tevelev <tevelevmaxim@gmail.com>
2024-07-30 10:55:30 +0300

    1

---
+++ L6/L6T1/math_functions.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double add(double a, double b) 
+{
+    return a + b;
+}
+
+double subtract(double a, double b) 
+{
+    return a - b;
+}
+
+double multiply(double a, double b) 
+{
+    return a * b;
+}
+
+double divide(double a, double b) 
+{
+    if (b == 0) {
+        cerr << "Ошибка: деление на ноль!" << endl;
+        return 0;
+    }
+    return a / b;
+}
+
+double power(double a, double b) 
+{
+    return pow(a, b);
+}
