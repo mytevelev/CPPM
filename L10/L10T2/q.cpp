54fcea446583116180a340abb981f231491c6d9a
Maxim Tevelev <tevelevmaxim@gmail.com>
2024-07-30 11:06:11 +0300

    1

---
+++ L10/L10T2/q.cpp
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <locale.h>
+#include <fstream>
+#include "fig.h"
+#include "q.h"
+using namespace std;
+
+
+
+Quadrangle::Quadrangle(int sideA, int sideB, int sideC, int sideD, int angleA, int angleB, int angleC, int angleD)
+    : Figure(4, "Четырёхугольник")
+{
+
+    this->sideA = sideA; this->sideB = sideB; this->sideC = sideC; this->sideD = sideD;
+    this->angleA = angleA;  this->angleB = angleB; this->angleC = angleC; this->angleD = angleD;
+    if (sideA <= 0 || sideB <= 0 || sideC <= 0 || sideD <= 0)
+    {
+        cerr << "Неверно задвны стороны треугольника " << endl;
+
+    }
+    if ((angleA <= 0 || angleB <= 0 || angleC <= 0 || angleD <= 0) || (angleA + angleB + angleC + angleD) != 360)
+    {
+        cerr << "Неверно заданы углы треугольника " << endl;
+
+    }
+
+};
+
+// Конструктор по умолчанию
+Quadrangle::Quadrangle() : Figure(4, "Четырёхугольник") {};
+
+
+bool Quadrangle::check()   ///// checking angles
+{
+    int angles = get_aA() + get_aB() + get_aC() + get_aD();
+    if (angles != 360) return true;
+
+    return false;
+};
+void Quadrangle::printinfo() 
+{
+    string err[] = { "OK","BAD" };
+
+    cout << err[check()] << endl;
+    cout << get_name() << ": " << get_sides_count()
+        << " side A: " << get_sA() << " side B: " << get_sB() << " side C: " << get_sC() << " side D: " << get_sD()
+        << " angle A: " << get_aA() << " angle B: " << get_aB() << " angle C: " << get_aC() << " angle D: " << get_aD()
+        << endl;
+
+}
---
+++ L10/L10T2/r.cpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <locale.h>
+#include <fstream>
+#include "fig.h"
+#include "q.h"
+#include "r.h"
+using namespace std;
+
+Rhombus::Rhombus(int sideA, int sideB, int sideC, int sideD, int angleA, int angleB, int angleC, int angleD) : /// тут вызвается конструктор родителя с параметрами
+    Quadrangle(sideA, sideB, sideC, sideD, angleA, angleB, angleC, angleD)
+{
+    if (!((sideA == sideB) && (sideC == sideB)))
+        cerr << "Неверно заданы стороны ромба. " << endl;
+    name = "Ромб";
+}; /// вот тут объект создан
+
+Rhombus::Rhombus() : Quadrangle(1, 1, 1, 1, 80, 100, 80, 100) { name = "Ромб"; }; // простой конструктор
+bool Rhombus::check()   ///// checking angles
+{
+    if (Quadrangle::check()) return true;
+    if ((get_aA() != get_aC()) || (get_aB() != get_aD())) return true;
+
+    return false;
+};
