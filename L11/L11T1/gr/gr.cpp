7150ab650235548e0bec9103b35c139977c329e9
Maxim Tevelev <tevelevmaxim@gmail.com>
2024-07-31 12:08:32 +0300

    1

---
+++ L11/L11T1/gr/gr.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include "Greeter.h"
+
+using namespace std;
+
+int main() {
+    // Установка кодировки консоли на UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    // Запрос имени пользователя
+    string name;
+    cout << "Введите имя: ";
+    getline(cin, name);
+
+    // Создание объекта класса Greeter и вызов метода greet
+    Greeter greeter;
+    string greeting = greeter.greet(name);
+
+    // Вывод приветствия
+    cout << greeting << endl;
+
+    return 0;
+}
---
+++ L11/L11T1/greeter.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef GREETER_H
+#define GREETER_H
+
+#include <string>
+
+class Greeter {
+public:
+    std::string greet(const std::string& name);
+};
+
+#endif // GREETER_H
