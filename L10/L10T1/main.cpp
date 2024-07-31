830adc6e39ba7e531fe622aeeebc6a4f6730ae03
Maxim Tevelev <tevelevmaxim@gmail.com>
2024-07-31 10:55:03 +0300

    2

--- L10/L10T1/main.cpp
+++ L10/L10T1/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <windows.h>
+#include <string>
 
-int main()
-{
+using namespace std;
 
+int main() {
+    // Установка кодировки консоли на UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
 
+    // Запрос имени пользователя
+    string name;
+    cout << "Введите имя: ";
+    getline(cin, name);
 
+    // Приветствие пользователя
+    cout << "Здравствуйте, " << name << "!" << endl;
+
+    return 0;
 }
